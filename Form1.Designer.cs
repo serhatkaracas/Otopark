d99d6961ac810045d150e3db6fb16a32461883c5
serhatkaracas <121132376+serhatkaracas@users.noreply.github.com>
2023-11-05 16:39:38 +0300

    bitti

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,6 +50,7 @@
             pictureBox16 = new PictureBox();
             checkBox16 = new CheckBox();
             button_kullanicicikis = new Button();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
@@ -63,9 +64,10 @@
             // checkBox1
             // 
             checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(197, 160);
+            checkBox1.Location = new Point(225, 213);
+            checkBox1.Margin = new Padding(3, 4, 3, 4);
             checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(32, 19);
+            checkBox1.Size = new Size(39, 24);
             checkBox1.TabIndex = 0;
             checkBox1.Text = "1";
             checkBox1.UseVisualStyleBackColor = true;
@@ -73,9 +75,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(197, 94);
+            pictureBox1.Location = new Point(225, 125);
+            pictureBox1.Margin = new Padding(3, 4, 3, 4);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(66, 50);
+            pictureBox1.Size = new Size(75, 67);
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
@@ -83,18 +86,20 @@
             // pictureBox2
             // 
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(308, 94);
+            pictureBox2.Location = new Point(352, 125);
+            pictureBox2.Margin = new Padding(3, 4, 3, 4);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(66, 50);
+            pictureBox2.Size = new Size(75, 67);
             pictureBox2.TabIndex = 3;
             pictureBox2.TabStop = false;
             // 
             // checkBox2
             // 
             checkBox2.AutoSize = true;
-            checkBox2.Location = new Point(308, 160);
+            checkBox2.Location = new Point(352, 213);
+            checkBox2.Margin = new Padding(3, 4, 3, 4);
             checkBox2.Name = "checkBox2";
-            checkBox2.Size = new Size(32, 19);
+            checkBox2.Size = new Size(39, 24);
             checkBox2.TabIndex = 2;
             checkBox2.Text = "2";
             checkBox2.UseVisualStyleBackColor = true;
@@ -102,18 +107,20 @@
             // pictureBox3
             // 
             pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
-            pictureBox3.Location = new Point(537, 94);
+            pictureBox3.Location = new Point(614, 125);
+            pictureBox3.Margin = new Padding(3, 4, 3, 4);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(66, 50);
+            pictureBox3.Size = new Size(75, 67);
             pictureBox3.TabIndex = 7;
             pictureBox3.TabStop = false;
             // 
             // checkBox3
             // 
             checkBox3.AutoSize = true;
-            checkBox3.Location = new Point(537, 160);
+            checkBox3.Location = new Point(614, 213);
+            checkBox3.Margin = new Padding(3, 4, 3, 4);
             checkBox3.Name = "checkBox3";
-            checkBox3.Size = new Size(32, 19);
+            checkBox3.Size = new Size(39, 24);
             checkBox3.TabIndex = 6;
             checkBox3.Text = "4";
             checkBox3.UseVisualStyleBackColor = true;
@@ -121,18 +128,20 @@
             // pictureBox4
             // 
             pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
-            pictureBox4.Location = new Point(426, 94);
+            pictureBox4.Location = new Point(487, 125);
+            pictureBox4.Margin = new Padding(3, 4, 3, 4);
             pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(66, 50);
+            pictureBox4.Size = new Size(75, 67);
             pictureBox4.TabIndex = 5;
             pictureBox4.TabStop = false;
             // 
             // checkBox4
             // 
             checkBox4.AutoSize = true;
-            checkBox4.Location = new Point(426, 160);
+            checkBox4.Location = new Point(487, 213);
+            checkBox4.Margin = new Padding(3, 4, 3, 4);
             checkBox4.Name = "checkBox4";
-            checkBox4.Size = new Size(32, 19);
+            checkBox4.Size = new Size(39, 24);
             checkBox4.TabIndex = 4;
             checkBox4.Text = "3";
             checkBox4.UseVisualStyleBackColor = true;
@@ -140,9 +149,10 @@
             // checkBox5
             // 
             checkBox5.AutoSize = true;
-            checkBox5.Location = new Point(546, 248);
+            checkBox5.Location = new Point(624, 331);
+            checkBox5.Margin = new Padding(3, 4, 3, 4);
             checkBox5.Name = "checkBox5";
-            checkBox5.Size = new Size(32, 19);
+            checkBox5.Size = new Size(39, 24);
             checkBox5.TabIndex = 14;
             checkBox5.Text = "1";
             checkBox5.UseVisualStyleBackColor = true;
@@ -150,9 +160,10 @@
             // checkBox6
             // 
             checkBox6.AutoSize = true;
-            checkBox6.Location = new Point(435, 248);
+            checkBox6.Location = new Point(497, 331);
+            checkBox6.Margin = new Padding(3, 4, 3, 4);
             checkBox6.Name = "checkBox6";
-            checkBox6.Size = new Size(32, 19);
+            checkBox6.Size = new Size(39, 24);
             checkBox6.TabIndex = 12;
             checkBox6.Text = "1";
             checkBox6.UseVisualStyleBackColor = true;
@@ -160,9 +171,10 @@
             // checkBox7
             // 
             checkBox7.AutoSize = true;
-            checkBox7.Location = new Point(317, 248);
+            checkBox7.Location = new Point(362, 331);
+            checkBox7.Margin = new Padding(3, 4, 3, 4);
             checkBox7.Name = "checkBox7";
-            checkBox7.Size = new Size(32, 19);
+            checkBox7.Size = new Size(39, 24);
             checkBox7.TabIndex = 10;
             checkBox7.Text = "1";
             checkBox7.UseVisualStyleBackColor = true;
@@ -170,9 +182,10 @@
             // checkBox8
             // 
             checkBox8.AutoSize = true;
-            checkBox8.Location = new Point(206, 248);
+            checkBox8.Location = new Point(235, 331);
+            checkBox8.Margin = new Padding(3, 4, 3, 4);
             checkBox8.Name = "checkBox8";
-            checkBox8.Size = new Size(32, 19);
+            checkBox8.Size = new Size(39, 24);
             checkBox8.TabIndex = 8;
             checkBox8.Text = "1";
             checkBox8.UseVisualStyleBackColor = true;
@@ -180,18 +193,20 @@
             // pictureBox13
             // 
             pictureBox13.Image = (Image)resources.GetObject("pictureBox13.Image");
-            pictureBox13.Location = new Point(537, 223);
+            pictureBox13.Location = new Point(614, 297);
+            pictureBox13.Margin = new Padding(3, 4, 3, 4);
             pictureBox13.Name = "pictureBox13";
-            pictureBox13.Size = new Size(66, 50);
+            pictureBox13.Size = new Size(75, 67);
             pictureBox13.TabIndex = 23;
             pictureBox13.TabStop = false;
             // 
             // checkBox13
             // 
             checkBox13.AutoSize = true;
-            checkBox13.Location = new Point(537, 289);
+            checkBox13.Location = new Point(614, 385);
+            checkBox13.Margin = new Padding(3, 4, 3, 4);
             checkBox13.Name = "checkBox13";
-            checkBox13.Size = new Size(32, 19);
+            checkBox13.Size = new Size(39, 24);
             checkBox13.TabIndex = 22;
             checkBox13.Text = "8";
             checkBox13.UseVisualStyleBackColor = true;
@@ -199,18 +214,20 @@
             // pictureBox14
             // 
             pictureBox14.Image = (Image)resources.GetObject("pictureBox14.Image");
-            pictureBox14.Location = new Point(426, 223);
+            pictureBox14.Location = new Point(487, 297);
+            pictureBox14.Margin = new Padding(3, 4, 3, 4);
             pictureBox14.Name = "pictureBox14";
-            pictureBox14.Size = new Size(66, 50);
+            pictureBox14.Size = new Size(75, 67);
             pictureBox14.TabIndex = 21;
             pictureBox14.TabStop = false;
             // 
             // checkBox14
             // 
             checkBox14.AutoSize = true;
-            checkBox14.Location = new Point(426, 289);
+            checkBox14.Location = new Point(487, 385);
+            checkBox14.Margin = new Padding(3, 4, 3, 4);
             checkBox14.Name = "checkBox14";
-            checkBox14.Size = new Size(32, 19);
+            checkBox14.Size = new Size(39, 24);
             checkBox14.TabIndex = 20;
             checkBox14.Text = "7";
             checkBox14.UseVisualStyleBackColor = true;
@@ -218,18 +235,20 @@
             // pictureBox15
             // 
             pictureBox15.Image = (Image)resources.GetObject("pictureBox15.Image");
-            pictureBox15.Location = new Point(308, 223);
+            pictureBox15.Location = new Point(352, 297);
+            pictureBox15.Margin = new Padding(3, 4, 3, 4);
             pictureBox15.Name = "pictureBox15";
-            pictureBox15.Size = new Size(66, 50);
+            pictureBox15.Size = new Size(75, 67);
             pictureBox15.TabIndex = 19;
             pictureBox15.TabStop = false;
             // 
             // checkBox15
             // 
             checkBox15.AutoSize = true;
-            checkBox15.Location = new Point(308, 289);
+            checkBox15.Location = new Point(352, 385);
+            checkBox15.Margin = new Padding(3, 4, 3, 4);
             checkBox15.Name = "checkBox15";
-            checkBox15.Size = new Size(32, 19);
+            checkBox15.Size = new Size(39, 24);
             checkBox15.TabIndex = 18;
             checkBox15.Text = "6";
             checkBox15.UseVisualStyleBackColor = true;
@@ -237,37 +256,53 @@
             // pictureBox16
             // 
             pictureBox16.Image = (Image)resources.GetObject("pictureBox16.Image");
-            pictureBox16.Location = new Point(197, 223);
+            pictureBox16.Location = new Point(225, 297);
+            pictureBox16.Margin = new Padding(3, 4, 3, 4);
             pictureBox16.Name = "pictureBox16";
-            pictureBox16.Size = new Size(66, 50);
+            pictureBox16.Size = new Size(75, 67);
             pictureBox16.TabIndex = 17;
             pictureBox16.TabStop = false;
             // 
             // checkBox16
             // 
             checkBox16.AutoSize = true;
-            checkBox16.Location = new Point(197, 289);
+            checkBox16.Location = new Point(225, 385);
+            checkBox16.Margin = new Padding(3, 4, 3, 4);
             checkBox16.Name = "checkBox16";
-            checkBox16.Size = new Size(32, 19);
+            checkBox16.Size = new Size(39, 24);
             checkBox16.TabIndex = 16;
             checkBox16.Text = "5";
             checkBox16.UseVisualStyleBackColor = true;
             // 
             // button_kullanicicikis
             // 
-            button_kullanicicikis.Location = new Point(197, 377);
+            button_kullanicicikis.BackColor = Color.FromArgb(255, 128, 128);
+            button_kullanicicikis.Location = new Point(832, 125);
+            button_kullanicicikis.Margin = new Padding(3, 4, 3, 4);
             button_kullanicicikis.Name = "button_kullanicicikis";
-            button_kullanicicikis.Size = new Size(75, 23);
+            button_kullanicicikis.Size = new Size(86, 31);
             button_kullanicicikis.TabIndex = 24;
             button_kullanicicikis.Text = "Çıkış";
-            button_kullanicicikis.UseVisualStyleBackColor = true;
+            button_kullanicicikis.UseVisualStyleBackColor = false;
             button_kullanicicikis.Click += button_kullanicicikis_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(128, 255, 128);
+            button1.Location = new Point(832, 75);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 25;
+            button1.Text = "Profil";
+            button1.UseVisualStyleBackColor = false;
             // 
             // Kullanici_paneli
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(973, 550);
+            BackColor = Color.FromArgb(128, 128, 255);
+            ClientSize = new Size(1112, 733);
+            Controls.Add(button1);
             Controls.Add(button_kullanicicikis);
             Controls.Add(pictureBox13);
             Controls.Add(checkBox13);
@@ -289,6 +324,7 @@
             Controls.Add(checkBox2);
             Controls.Add(pictureBox1);
             Controls.Add(checkBox1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Kullanici_paneli";
             Text = "Kulanıcı paneli";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -326,5 +362,6 @@
         private PictureBox pictureBox16;
         private CheckBox checkBox16;
         private Button button_kullanicicikis;
+        private Button button1;
     }
 }
